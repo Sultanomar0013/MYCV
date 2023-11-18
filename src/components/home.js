b7604786439cfb54c2ee7497ef4292796280c1dc
Sultanomar0013 <sultanomar756@gmail.com>
2023-11-18 10:14:12 +0600

    about me page add

--- src/components/home.js
+++ src/components/home.js
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../css/home.css';
-import TheStarryNightBlack from '../images/TheStarryNightBlack.jpeg';
+import $ from 'jquery';
+import 'tilt.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from "../images/profile.png"
+import Typed from "typed.js";
 
 function Home() {
+    useEffect(() => {
+        $('.tiltcard').tilt({
+            max: 25,
+            speed: 400,
+            glare: false,
+            "max-glare": 0.5
+        });
 
-    const backgroundStyle = {
-        backgroundImage: `url(${TheStarryNightBlack})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        width: '100%',
-    };
+        var typed = new Typed(".auto-type", {
+            strings: [" Welcome to my portfolio!", "Have A Chill!!!"],
+            typeSpeed: 150,
+            backSpeed: 150,
+            loop: true,
+        });
+        return () => {
+            typed.destroy();
+            $('.tiltcard').tilt('destroy');
+        };
+
+    }, []);
+
+
 
     return (
         <>
-            <div className="container-fluid p-0 " style={backgroundStyle}>
-                <div className="titletextdiv">
-                    <p className="text-white titletext">hello</p>
+            <section>
+                <div className=" textdiv " >
+                    <div className="container content text-3d-shadow">
+                        <h1>Hey There!!! My name is Omar Saleh Sultan,  <span className="auto-type" style={{ color: "chocolate" }}></span></h1>
+
+                    </div>
+                    <div className="container imgdiv">
+                        <img src={Profile} alt="Profile IMG" className="profile-image" />
+                    </div>
                 </div>
-            
-            </div>
+            </section>
+
+            <section className="aboutmeSection">
+                <div className="tiltcard" data-tilt>
+                    <div className="tilttextdiv">
+                        <h1 >helloWorld</h1>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 }
